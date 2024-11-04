ef64bd69ff7a62cf45d02c47ef3672da48f214ef
Noah <noah.copiak+apple@gmail.com>
2024-11-04 15:11:14 -0700

    fix: update font file paths to correct relative path

--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -1,23 +1,23 @@
 import localFont from "next/font/local";
 
 export const ppMoriSemiBold = localFont({
-  src: "../../public/fonts/PPMori-Semibold.woff",
+  src: "../../../fonts/PPMori-Semibold.woff",
   variable: "--font-pp-mori-semibold",
 });
 
 export const junicodeBoldItalic = localFont({
-  src: "../../public/fonts/junicode-bolditalic-webfont.woff",
+  src: "../../../fonts/junicode-bolditalic-webfont.woff",
   variable: "--font-junicode-bold-italic",
 });
 
 export const geistSans = localFont({
-  src: "../../public/fonts/GeistVF.woff",
+  src: "../../../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 
 export const chesterPro = localFont({
-  src: "../../public/fonts/BNChesterPro.woff",
+  src: "../../../fonts/BNChesterPro.woff",
   variable: "--font-chester-pro",
   weight: "400",
 });
