e4437c162d050984300674a323d407d0f3766288
Noah <noah.copiak+apple@gmail.com>
2024-11-04 15:48:50 -0700

    fix: update font configuration for Vercel deployment

--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -1,28 +1,28 @@
 import localFont from "next/font/local";
 
 export const ppMoriSemiBold = localFont({
-  src: './files/PPMori-Semibold.woff',
+  src: '../../public/fonts/PPMori-Semibold.woff',
   variable: "--font-pp-mori-semibold",
   weight: '600',
   style: 'normal',
 });
 
 export const junicodeBoldItalic = localFont({
-  src: './files/junicode-bolditalic-webfont.woff',
+  src: '../../public/fonts/junicode-bolditalic-webfont.woff',
   variable: "--font-junicode-bold-italic",
   weight: '700',
   style: 'italic',
 });
 
 export const geistSans = localFont({
-  src: './files/GeistVF.woff',
+  src: '../../public/fonts/GeistVF.woff',
   variable: "--font-geist-sans",
   weight: '100 900',
   style: 'normal',
 });
 
 export const chesterPro = localFont({
-  src: './files/BNChesterPro.woff',
+  src: '../../public/fonts/BNChesterPro.woff',
   variable: "--font-chester-pro",
   weight: '400',
   style: 'normal',
