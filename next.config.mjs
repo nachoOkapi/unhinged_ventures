c08c138548a65ad60956400aae8bc12382504400
Noah <noah.copiak+apple@gmail.com>
2024-11-04 15:01:30 -0700

    Fix font paths and webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.(woff|woffeot|ttf|otf)$/i,
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
       type: 'asset/resource',
     });
     return config;
--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -1,23 +1,23 @@
 import localFont from "next/font/local";
 
 export const ppMoriSemiBold = localFont({
-  src: "../../public/fonts/PPMori-Semibold.woff",
+  src: "../../../public/fonts/PPMori-Semibold.woff",
   variable: "--font-pp-mori-semibold",
 });
 
 export const junicodeBoldItalic = localFont({
-  src: "../../public/fonts/junicode-bolditalic-webfont.woff",
+  src: "../../../public/fonts/junicode-bolditalic-webfont.woff",
   variable: "--font-junicode-bold-italic",
 });
 
 export const geistSans = localFont({
-  src: "../../public/fonts/GeistVF.woff",
+  src: "../../../public/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 
 export const chesterPro = localFont({
-  src: "../../public/fonts/BNChesterPro.woff",
+  src: "../../../public/fonts/BNChesterPro.woff",
   variable: "--font-chester-pro",
   weight: "400",
-}); 
+});
